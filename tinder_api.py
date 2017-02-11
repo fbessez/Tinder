b48a9cbf32335236dc3fd24fbd51587ea57ff4c4
Fabien Bessez <fabien.bessez@gmail.com>
2017-02-11 14:16:39 -0500

    superlike, like, dislike, report

--- tinder_api.py
+++ tinder_api.py
@@ -21,9 +21,9 @@ Known endpoints:
 -- https://api.gotinder.com/profile
 -- https://api.gotinder.com/meta
 https://api.gotinder.com/user/ping
-https://api.gotinder.com/passport/user/travel
-https://api.gotinder.com/{like|pass}/{_id}
+-- https://api.gotinder.com/{like|pass}/{_id}
 https://api.gotinder.com/group/{like|pass}/{id}
+https://api.gotinder.com/passport/user/travel
 https://api.gotinder.com/report/{_id}
 """
 
@@ -35,6 +35,7 @@ def get_auth_token(fb_auth_token, fb_user_id):
     	)
 	try:
 		return req.json()["token"]
+		headers.update({"X-Auth-Token": tinder_auth_token})
 	except:
 		return {"error": "could not authorize"}
 
@@ -45,7 +46,6 @@ tinder_auth_token = get_auth_token(config.fb_auth_token, config.fb_user_id)
 # I think recommended means the users who have already 'liked' you
 # r.json()["results"][index]["name"] == the person at index's name
 def get_recommendations():
-	headers.update({"X-Auth-Token": tinder_auth_token})
 	try:
 		r = requests.get('https://api.gotinder.com/user/recs', headers=headers)
 		return r.json()
@@ -58,7 +58,6 @@ def get_recommendations():
 # OUTPUT: A dict of all updates since given activity date
 # NOTES: You can do this to get messages sent, all matches...
 def get_updates(last_activity_date=""):
-	headers.update({"X-Auth-Token": tinder_auth_token})
 	r = requests.post('https://api.gotinder.com/updates',
 		headers=headers,
 		data=json.dumps({"last_activity_date": last_activity_date}))
@@ -73,7 +72,6 @@ def get_updates(last_activity_date=""):
 ## 'pos_info', 'jobs', 'blend', 'distance_filter', '_id', 
 ## 'create_date']
 def get_self():
-	headers.update({"X-Auth-Token": tinder_auth_token})
 	url = config.host + '/profile'
 	r = requests.get(url, headers=headers)
 	return r.json()
@@ -83,16 +81,15 @@ def get_self():
 ## 'status', 'groups', 'products', 'rating', 'tutorials', 
 ## 'travel', 'notifications', 'user']
 def get_meta():
-	headers.update({"X-Auth-Token": tinder_auth_token})
 	url = config.host + '/meta'
 	r = requests.get(url, headers=headers)
 	return r.json()
 
-# def get_ping():
-# 	headers.update({"X-Auth-Token": tinder_auth_token})
-# 	url = config.host + '/user/ping'
-# 	r = requests.post(url, headers=headers)
-# 	return r.json()
+def get_ping(lat, lon):
+	headers.update({"X-Auth-Token": tinder_auth_token})
+	url = config.host + '/user/ping'
+	r = requests.post(url, headers=headers)
+	return r.json()
 
 # Input: person_id --> the tinder_id of a user not yourself
 # Output: The person_id's corresponding profile information
@@ -100,25 +97,43 @@ def get_meta():
 
 # NEED A FUNCTION TO GET THAT PERSON'S ID FROM THE UPDATES CALL
 def get_person(person_id):
-	headers.update({"X-Auth-Token": tinder_auth_token})
 	url = config.host + '/user/%s' % person_id
 	r = requests.get(url, headers=headers)
 	return r.json()
 
-
-
-# INPUT: match_id -> this is different than the person's person_id
+# INPUT: match_id : string -> 
+#	this is different than the person's person_id
 #	the match_id is typically an id for the chat
 # 	usually, match_id = person1_id ^ person2_id
 # 	get_updates["matches"][0]["_id"] is the match_id for match 0, the index
-# INPUT: msg -> Desired message to be sent
+# INPUT: msg : string -> 
+#	Desired message to be sent
 # OUTPUT: Success or Failure msg
 def send_msg(match_id, msg):
-	headers.update({"X-Auth-Token": tinder_auth_token})
 	url = config.host + '/user/matches/%s' % match_id
 	r = requests.get(url, headers=headers, data=json.dumps({"message": msg}))
 	return r.json()
 
+def superlike(person_id):
+	url = config.host + '/like/%s/super' % person_id
+	r = requests.get(url, headers=headers)
+	return r.json()	
+
+def like(person_id):
+	url = config.host + '/like/%s' % person_id
+	r = requests.get(url, headers=headers)
+	return r.json()
+
+def dislike(person_id):
+	url = config.host + '/pass/%s' % person_id
+	r = requests.get(url, headers=headers)
+	return r.json()
+
+# Cause must be one of the given options
+# I WILL CHECK THESE LATER
+def report(person_id, cause):
+	url = config.host + '/report/%s' % person_id
+	r = requests.post(url, headers=headers, data={"cause": cause})
 
 
 
