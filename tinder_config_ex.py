77592bc455125a3ea3be05869d50874550b6f6a0
Fabien Bessez <fabien.bessez@gmail.com>
2017-02-17 18:15:02 -0500

    Updating explanation

--- tinder_config_ex.py
+++ tinder_config_ex.py
@@ -2,3 +2,8 @@ fb_auth_token = <fb_token>
 fb_user_id = <id>
 
 
+# Your real config file should simply be named "config.py"
+# and you will insert your fb_auth_token and fb_user_id by
+# following the guidelines in the README. NOTE: As of now,
+# the fb_auth_token only lasts a few hours before it expires
+# and you must follow the procedure again to get a new one.
