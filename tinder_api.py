7f17f9edf3c9afdd0ecd6102127b2fab483d99d9
Fabien Bessez <fabien.bessez@gmail.com>
2017-07-14 00:19:52 +0000

    testing merge atom

--- tinder_api.py
+++ tinder_api.py
@@ -10,7 +10,7 @@ headers = {
     'app_version': '6.9.4',
     'platform': 'ios',
     "content-type": "application/json",
-    "User-agent": "Tinder/4.7.1 (iPhone; iOS 9.2; Scale/2.00)",
+    "User-agent": "Tinder/7.5.3 (iPhone; iOS 10.3.2; Scale/2.00)",
 }
 
 """
