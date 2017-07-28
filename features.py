e89c916c23da4893fa7f208deb9811d16fa0abf5
Fabien Bessez <fabien.bessez@gmail.com>
2017-07-28 20:16:02 +0000

    refactoring

---
+++ features.py
@@ -0,0 +1,170 @@
+# coding=utf-8
+
+from datetime import date, datetime
+from random import random
+from time import sleep
+
+import config
+import tinder_api as api
+
+
+'''
+This file collects important data on your matches,
+allows you to sort them by last_activity_date, age,
+gender, message count, and their average successRate.
+'''
+
+
+def get_match_info():
+    matches = api.get_updates()['matches']
+    now = datetime.utcnow()
+    match_info = {}
+    # TODO: Why is this not just all matches?
+    for match in matches[:len(matches) - 2]:
+        try:
+            person = match['person']
+            person_id = person['_id']  # This ID for looking up person
+            match_info[person_id] = {
+                "name": person['name'],
+                "match_id": match['id'],  # This ID for messaging
+                "message_count": match['message_count'],
+                "photos": get_photos(person),
+                "bio": person['bio'],
+                "gender": person['gender'],
+                "avg_successRate": get_avg_successRate(person),
+                "messages": match['messages'],
+                "age": calculate_age(match['person']['birth_date']),
+                "distance": api.get_person(person_id)['results']['distance_mi'],
+                "last_activity_date": match['last_activity_date'],
+            }
+        except Exception as ex:
+            template = "An exception of type {0} occurred. Arguments:\n{1!r}"
+            message = template.format(type(ex).__name__, ex.args)
+            print(message)
+            # continue
+    print("All data stored in: match_info")
+    return match_info
+
+
+def get_match_id_by_name(name):
+    '''
+    Returns a list_of_ids that have the same name as your input
+    '''
+    global match_info
+    list_of_ids = []
+    for match in match_info:
+        if match_info[match]['name'] == name:
+            list_of_ids.append(match_info[match]['match_id'])
+    if len(list_of_ids) > 0:
+        return list_of_ids
+    return {"error": "No matches by name of %s" % name}
+
+
+def get_photos(person):
+    '''
+    Returns a list of photo urls
+    '''
+    photos = person['photos']
+    photo_urls = []
+    for photo in photos:
+        photo_urls.append(photo['url'])
+    return photo_urls
+
+
+def calculate_age(birthday_string):
+    '''
+    Converts from '1997-03-25T22:49:41.151Z' to an integer (age)
+    '''
+    birthyear = int(birthday_string[:4])
+    birthmonth = int(birthday_string[5:7])
+    birthday = int(birthday_string[8:10])
+    today = date.today()
+    return today.year - birthyear - ((today.month, today.day) < (birthmonth, birthday))
+
+
+def get_avg_successRate(person):
+    '''
+    SuccessRate is determined by Tinder for their 'Smart Photos' feature
+    '''
+    photos = person['photos']
+    curr_avg = 0
+    for photo in photos:
+        try:
+            photo_successRate = photo['successRate']
+            curr_avg += photo_successRate
+        except:
+            return -1
+    return curr_avg / len(photos)
+
+
+def sort_by_value(sortType):
+    '''
+    Sort options are:
+        'age', 'message_count', 'successRate', 'gender', 'activity_date'
+    '''
+    global match_info
+    return sorted(match_info.items(), key=lambda x: x[1][sortType], reverse=True)
+
+
+def see_friends_profiles(name=None):
+    friends = api.see_friends()
+    if name == None:
+        return friends
+    else:
+        result_dict = {}
+        name = name.title()  # upcases first character of each word
+        for friend in friends:
+            if name in friend["name"]:
+                result_dict[friend["name"]] = friend
+        if result_dict == {}:
+            return "No friends by that name"
+        return result_dict
+
+
+def convert_from_datetime(difference):
+    secs = difference.seconds
+    days = difference.days
+    m, s = divmod(secs, 60)
+    h, m = divmod(m, 60)
+    return ("%d days, %d hrs %02d min %02d sec" % (days, h, m, s))
+
+
+def get_last_activity_date(now, ping_time):
+    ping_time = ping_time[:len(ping_time) - 5]
+    datetime_ping = datetime.strptime(ping_time, '%Y-%m-%dT%H:%M:%S')
+    difference = now - datetime_ping
+    since = convert_from_datetime(difference)
+    return since
+
+
+def how_long_has_it_been():
+    global match_info
+    now = datetime.utcnow()
+    times = {}
+    for person in match_info:
+        name = match_info[person]['name']
+        ping_time = match_info[person]['last_activity_date']
+        since = get_last_activity_date(now, ping_time)
+        times[name] = since
+        print(name, "----->", since)
+    return times
+
+
+def pause():
+    '''
+    In order to appear as a real Tinder user using the app...
+    When making many API calls, it is important to pause a...
+    realistic amount of time between actions to not make Tinder...
+    suspicious!
+    '''
+    nap_length = 5 * random()
+    print('Napping for %f seconds...' % nap_length)
+    sleep(nap_length)
+
+
+if __name__ == 'main':
+    if api.authverif() == True:
+        print("Gathering Data on your matches...")
+        match_info = get_match_info()
+    else:
+        print("Something went wrong. You were not authorized.")
--- tinder_api.py
+++ tinder_api.py
@@ -4,36 +4,12 @@ import json
 import config
 import requests
 
-# from datetime import datetime
-
 headers = {
     'app_version': '6.9.4',
     'platform': 'ios',
     "content-type": "application/json",
     "User-agent": "Tinder/7.5.3 (iPhone; iOS 10.3.2; Scale/2.00)",
 }
-
-"""
-Known endpoints:
-    tested:
-        -- https://api.gotinder.com/auth
-        -- https://api.gotinder.com/user/recs
-        -- https://api.gotinder.com/user/matches/_id
-        -- https://api.gotinder.com/user/_id
-        -- https://api.gotinder.com/updates
-        -- https://api.gotinder.com/profile
-        -- https://api.gotinder.com/meta
-        -- https://api.gotinder.com/user/ping
-        -- https://api.gotinder.com/report/{_id}
-        -- https://api.gotinder.com/{like|pass}/{_id}
-        -- https://api.gotinder.com/matches/{match_id}
-
-    untested:
-        https://api.gotinder.com/group/{like|pass}/{id}
-        https://api.gotinder.com/passport/user/travel
-        https://api.gotinder.com/message/{message_id}
-
-"""
 
 
 def get_auth_token(fb_auth_token, fb_user_id):
@@ -50,11 +26,10 @@ def get_auth_token(fb_auth_token, fb_user_id):
     try:
         tinder_auth_token = req.json()["token"]
         headers.update({"X-Auth-Token": tinder_auth_token})
-        print("Greetings, you have been authorized!")
-        # config.authorized = True
+        print("You have been successfully authorized!")
         return tinder_auth_token
     except:
-        return {"error": "Something went wrong. Sorry."}
+        return {"error": "Something went wrong. Sorry, but we could not authorize you."}
 
 
 def authverif():
@@ -63,229 +38,178 @@ def authverif():
         return False
     return True
 
-# INPUT : None
-# OUTPUT: A dict of the recommended users to swipe on
-# I think recommended means the users who have already 'liked' you
-# r.json()["results"][index]["name"] == the person at index's name
-
 
 def get_recommendations():
+    '''
+    Returns a list of users that you can swipe on
+    '''
     try:
         r = requests.get('https://api.gotinder.com/user/recs', headers=headers)
         return r.json()
-    except:
-        return {"error": "could not retrieve recommendations"}
-
-        # COULD DO LIKE IF "ERROR" IN r.json() then print this else do this.
+    except requests.exceptions.RequestException as e:
+        print("Something went wrong with getting recomendations:", e)
 
 
-# INPUT: last_activity_date -> the date from which you want to see updates
-#                            -> defaults to "" which means from the beginning of time
-#         should be of the form ______________________
-# OUTPUT: A dict of all updates since given activity date
-# NOTES: You can do this to get messages sent, all matches...
 def get_updates(last_activity_date=""):
-    r = requests.post('https://api.gotinder.com/updates',
-                      headers=headers,
-                      data=json.dumps({"last_activity_date": last_activity_date}))
-    return r.json()
-
-# Returns the following keys:
-# ['squads_only', 'squads_discoverable', 'instagram',
-## 'gender', 'bio', 'birth_date', 'name', 'interested_in',
-## 'location', 'gender_filter', 'age_filter_min', 'photos',
-## 'ping_time', 'squad_ads_shown', 'discoverable', 'facebook_id',
-## 'pos', 'can_create_squad', 'age_filter_max', 'schools',
-## 'pos_info', 'jobs', 'blend', 'distance_filter', '_id',
-# 'create_date']
+    '''
+    Returns all updates since the given activity date.
+    The last activity date is defaulted at the beginning of time.
+    Format for last_activity_date: "2017-07-09T10:28:13.392Z"
+    '''
+    try:
+        url = config.host + '/updates'
+        r = requests.post(url,
+                          headers=headers,
+                          data=json.dumps({"last_activity_date": last_activity_date}))
+        return r.json()
+    except requests.exceptions.RequestException as e:
+        print("Something went wrong with getting updates:", e)
 
 
 def get_self():
-    url = config.host + '/profile'
-    r = requests.get(url, headers=headers)
-    return r.json()
+    '''
+    Returns your own profile data
+    '''
+    try:
+        url = config.host + '/profile'
+        r = requests.get(url, headers=headers)
+        return r.json()
+    except requests.exceptions.RequestException as e:
+        print("Something went wrong. Could not get your data:", e)
 
 
-# 18 >= age_filter_min <= 46
-# 22 >- age_filter_max <= 55
-# age_filter_min <= age_filter_max - 4
-# gender 0 = looking for males, gender 1 = looking for females
-# 1 <= distance_filter >= 100 in miles
 def change_preferences(**kwargs):
-    url = config.host + '/profile'
-    r = requests.post(url, headers=headers, data=json.dumps(kwargs))
-    return r.json()
-
-# Returns metadata containing the following keys:
-# ['globals', 'client_resources', 'versions', 'purchases',
-## 'status', 'groups', 'products', 'rating', 'tutorials',
-# 'travel', 'notifications', 'user']
+    '''
+    kwargs: a dictionary - whose keys become separate keyword arguments and the values become values of these arguments
+    age_filter_min: 18..46
+    age_filter_max: 22..55
+    age_filter_min <= age_filter_max - 4
+    gender: 0 == seeking males, 1 == seeking females
+    distance_filter: 1..100
+    '''
+    try:
+        url = config.host + '/profile'
+        r = requests.post(url, headers=headers, data=json.dumps(kwargs))
+        return r.json()
+    except requests.exceptions.RequestException as e:
+        print("Something went wrong. Could not change your preferences:", e)
 
 
 def get_meta():
-    url = config.host + '/meta'
-    r = requests.get(url, headers=headers)
-    return r.json()
+    '''
+    Returns meta data on yourself. Including the following keys:
+    ['globals', 'client_resources', 'versions', 'purchases',
+    'status', 'groups', 'products', 'rating', 'tutorials',
+    'travel', 'notifications', 'user']
+    '''
+    try:
+        url = config.host + '/meta'
+        r = requests.get(url, headers=headers)
+        return r.json()
+    except requests.exceptions.RequestException as e:
+        print("Something went wrong. Could not get your metadata:", e)
 
 
 def update_location(lat, lon):
-    # headers.update({"X-Auth-Token": tinder_auth_token})
-    url = config.host + '/user/ping'
-    r = requests.get(url, headers=headers, data={"lat": lat, "lon": lon})
-    return r.json()
+    '''
+    Updates your location to the given float inputs
+    Note: Requires a passport / Tinder Plus)
+    '''
+    try:
+        url = config.host + '/user/ping'
+        r = requests.get(url, headers=headers, data={"lat": lat, "lon": lon})
+        return r.json()
+    except requests.exceptions.RequestException as e:
+        print("Something went wrong. Could not update your location:", e)
 
-# Input: person_id --> the tinder_id of a user not yourself
-# Output: The person_id's corresponding profile information
-#        bio, pictures, gender, birthdate...
 
-# NEED A FUNCTION TO GET THAT PERSON'S ID FROM THE UPDATES CALL
-
-
-def get_person(person_id):
-    url = config.host + '/user/%s' % person_id
-    r = requests.get(url, headers=headers)
-    return r.json()
-
-# INPUT: match_id : string ->
-#    this is different than the person's person_id
-#    the match_id is typically an id for the chat
-#     usually, match_id = person1_id ^ person2_id
-#     get_updates["matches"][0]["_id"] is the match_id for match 0, the index
-# INPUT: msg : string ->
-#    Desired message to be sent
-# OUTPUT: Success or Failure msg
+def get_person(id):
+    '''
+    Gets a user's profile via their id
+    '''
+    try:
+        url = config.host + '/user/%s' % id
+        r = requests.get(url, headers=headers)
+        return r.json()
+    except requests.exceptions.RequestException as e:
+        print("Something went wrong. Could not get that person:", e)
 
 
 def send_msg(match_id, msg):
-    url = config.host + '/user/matches/%s' % match_id
-    r = requests.post(url, headers=headers, data=json.dumps({"message": msg}))
-    return r.json()
+    try:
+        url = config.host + '/user/matches/%s' % match_id
+        r = requests.post(url, headers=headers,
+                          data=json.dumps({"message": msg}))
+        return r.json()
+    except requests.exceptions.RequestException as e:
+        print("Something went wrong. Could not send your message:", e)
 
 
 def superlike(person_id):
-    url = config.host + '/like/%s/super' % person_id
-    r = requests.get(url, headers=headers)
-    return r.json()
+    try:
+        url = config.host + '/like/%s/super' % person_id
+        r = requests.get(url, headers=headers)
+        return r.json()
+    except requests.exceptions.RequestException as e:
+        print("Something went wrong. Could not superlike:", e)
 
 
 def like(person_id):
-    url = config.host + '/like/%s' % person_id
-    r = requests.get(url, headers=headers)
-    return r.json()
+    try:
+        url = config.host + '/like/%s' % person_id
+        r = requests.get(url, headers=headers)
+        return r.json()
+    except requests.exceptions.RequestException as e:
+        print("Something went wrong. Could not like:", e)
 
 
 def dislike(person_id):
-    url = config.host + '/pass/%s' % person_id
-    r = requests.get(url, headers=headers)
-    return r.json()
+    try:
+        url = config.host + '/pass/%s' % person_id
+        r = requests.get(url, headers=headers)
+        return r.json()
+    except requests.exceptions.RequestException as e:
+        print("Something went wrong. Could not dislike:", e)
 
-# Cause must be one of the given options
 
-
-def report(person_id, cause):
-    url = config.host + '/report/%s' % person_id
-    r = requests.post(url, headers=headers, data={"cause": cause})
-    return r.json()
+def report(person_id, cause, explanation=''):
+    '''
+    There are three options for cause:
+        0 : Other and requires an explanation
+        1 : Feels like spam and no explanation
+        4 : Inappropriate Photos and no explanation
+    '''
+    try:
+        url = config.host + '/report/%s' % person_id
+        r = requests.post(url, headers=headers, data={
+                          "cause": cause, "text": explanation})
+        return r.json()
+    except requests.exceptions.RequestException as e:
+        print("Something went wrong. Could not report:", e)
 
 
 def match_info(match_id):
-    url = config.host + '/matches/%s' % match_id
-    r = requests.get(url, headers=headers)
-    return r.json()
+    try:
+        url = config.host + '/matches/%s' % match_id
+        r = requests.get(url, headers=headers)
+        return r.json()
+    except requests.exceptions.RequestException as e:
+        print("Something went wrong. Could not get your match info:", e)
 
 
 def see_history():
-    url = config.host + '/tinder/history'
-    r = requests.post(url, headers=headers, data={'last_activity_date': ''})
-    return r.json()
+    try:
+        url = config.host + '/tinder/history'
+        r = requests.post(url, headers=headers, data={'last_activity_date': ''})
+        return r.json()
+    except requests.exceptions.RequestException as e:
+        print("Something went wrong. Could not see your history:", e)
 
-
-# def message_by_id(message_id):
-#     url = config.host + '/matches/%s' % message_id
-#     r = requests.get(url, headers=headers)
-#     return r.json()
-
-# See all friends of yours that have Tinder
-# ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! !
 
 def see_friends():
-    url = "https://api.gotinder.com/group/friends"
-    r = requests.get(url, headers=headers)
-    return r.json()['results']
-# Ólafur Jóhann Ólafsson
-
-#################################
-#################################
-#################################
-#################################
-#################################
-#################################
-# More abstract version of these API calls
-
-
-def get_url(keyword, required_input):
-    urls = {
-        "recs": "https://api.gotinder.com/user/recs",
-        "person": "https://api.gotinder.com/user/%s" % required_input,
-        "profile": "https://api.gotinder.com/profile",
-        "meta": "https://api.gotinder.com/meta",
-        "like": "https://api.gotinder.com/like/%s" % required_input,
-        "pass": "https://api.gotinder.com/pass/%s" % required_input,
-        "superlike": "https://api.gotinder.com/like/%s/super",
-        "match": "https://api.gotinder.com/matches/%s" % required_input,
-        "friends": "https://api.gotinder.com/group/friends",
-    }
-    return urls[keyword]
-
-
-def get(keyword, required_input, message=None):
-    url = get_url(keyword, required_input)
-    r = requests.get(url, headers=headers, data=message)
-    return r.json()
-
-
-def post_url(keyword, required_input):
-    urls = {
-        "send_msg": "https://api.gotinder.com/user/matches/%s" % required_input,
-        "auth": "https://api.gotinder.com/auth",
-        "ping": "https://api.gotinder.com/user/ping",
-        "updates": "https://api.gotinder.com/updates",
-        "profile": "https://api.gotinder.com/profile",
-        "report": "https://api.gotinder.com/report/%s" % required_input
-    }
-    return urls[keyword]
-
-
-def dataformatter(keyword, data):
-    if keyword == "send_msg":
-        return json.dumps({"message": data[0]})
-        # data[0] = message
-    elif keyword == "auth":
-        return json.dumps({'facebook_token': data[0], 'facebook_id': data[1]})
-        # data[0] = fb_auth_token, data[1] = fb_user_id
-    elif keyword == "ping":
-        return json.dumps({"lat": data[0], "lon": data[1]})
-        # data[0] = latitude, data[1] = longitude
-    elif keyword == "updates":
-        return json.dumps({"last_activity_date": data[0]})
-        # data[0] = timestamp
-    elif keyword == "profile":
-        return json.dumps({"age_filter_min": data[0], "gender_filter": data[1], "gender": data[2], "age_filter_max": data[3], "distance_filter": data[4]})
-        # data[0] = age_filter_min, data[1] = gender_filter, data[2] = gender, data[3] = age_filter_max, data[4] = distance_filter
-        # 18 >= age_filter_min <= 46
-        # 22 >- age_filter_max <= 55
-        # age_filter_min <= age_filter_max - 4
-        # gender 0 = looking for males, gender 1 = looking for females
-        # 1 <= distance_filter >= 100 in miles
-    elif keyword == "report":
-        return json.dumps({"cause": data[0]})
-        # data[0] = reason
-    else:
-        return None
-
-
-def post(keyword, required_input, data=None):
-    url = post_url(keyword, required_input)
-    data = dataformatter(keyword, data)
-    r = requests.post(url, headers=headers, data=data)
-    return r.json()
+    try:
+        url = config.host + '/group/friends'
+        r = requests.get(url, headers=headers)
+        return r.json()['results']
+    except requests.exceptions.RequestException as e:
+        print("Something went wrong. Could not get your Facebook friends:", e)
