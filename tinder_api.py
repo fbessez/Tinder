1a7c969531205a34dbdc01c37e42a4f9f3081c49
Eric Murta <ericmurta@hotmail.com>
2017-12-08 14:39:22 -0300

    Update tinder_api.py

--- tinder_api.py
+++ tinder_api.py
@@ -161,7 +161,7 @@ def get_person(id):
 def send_msg(match_id, msg):
     try:
         my_id = get_self()['_id']
-        url = config.host + '/user/matches/%s' % match_id + my_id
+        url = config.host + '/user/matches/%s%s' % (match_id, my_id)
         r = requests.post(url, headers=headers,
                           data=json.dumps({"message": msg}))
         return r.json()
