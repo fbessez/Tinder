69298b8779b61a38d36064d71ff7437b52510641
kurtmaege <kurtmaege@yahoo-inc.com>
2018-01-27 09:37:02 -0800

    fixed error on non ascii chars. bunch of output and most updated version

--- main.py
+++ main.py
@@ -11,7 +11,8 @@ def main():
     print("likes remaining: {0}".format(features.get_likes_remaining()))
 #    str = unicode('\x80abc', errors='replace')
 #    print(str.encode('utf-8'))
-#    like_all_nonad_recs()
+    for i in range(0, 20):
+        like_all_nonad_recs()
 
 def like_all_nonad_recs():
     get_recs = api.get_recs_v2()
@@ -24,17 +25,30 @@ def like_all_nonad_recs():
         bio = user['bio']
         name = user['name'].encode('utf-8')
         id = user['_id']
-        print(u'name: {0}, id: {1}'.format(name, id))
+        try:
+            print(u'name: {0}, id: {1}'.format(name, id))
+        except UnicodeDecodeError as err:
+            print(u'name: throws UnicodeDecodeError so I cannot say, id: {1}'.format(name, id))
+            print("UnicodeDecodeError: {0}".format(err))
+
         if 'is_brand' in user and user['is_brand']:
-            print(u'\tis_brand, will left-swipe now {0}'.format(api.dislike_and_pause(id)))
+            print(u'\tis_brand, will left-swipe now {0}'.format(features.dislike_and_pause(id)))
         else:
-            like_response = features.like_and_pause(id)
+            try:
+                like_response = features.like_and_pause(id)
+            except ConnectionError as connectionError:
+                print("probably ran out of likes")
+                print("ConnectionError: {0}".format(connectionError))
+                return;
             print(u'\tliking... {0}'.format(like_response))
             if like_response['match']:
                 print('It\'s a match!')
-                print(api.send_msg(id, opener_message))
+               #print(api.send_msg(id, opener_message))
             else:
                 print('Aww, it\'s not a match:(')
+            if like_response['likes_remaining'] == 0:
+                print("out of likes. Closing...")
+                return;
 
 
 if __name__ == '__main__':
