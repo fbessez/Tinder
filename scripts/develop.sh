de4af4adcc2b0e3e2c831911d62435689644ef3f
Vikash Kothary <kothary.vikash@gmail.com>
2018-10-18 19:39:02 +0100

    Update virtualenv path to hidden folder

--- scripts/develop.sh
+++ scripts/develop.sh
@@ -1,5 +1,5 @@
 #sudo apt-get install python3-pip python3-venv
 #sudo pip3 install virtualenv
-python3 -m venv venv
-venv/bin/pip3 install -r requirements.txt
+python3 -m venv .venv
+.venv/bin/pip3 install -r requirements.txt
 #source venv/bin/activate
