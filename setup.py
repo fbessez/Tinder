3fbab4d18a75e3839f3ba1d4ad8a646ad76b2e1a
Vikash Kothary <kothary.vikash@gmail.com>
2018-10-18 21:09:38 +0100

    Extract package variables for easy editing

--- setup.py
+++ setup.py
@@ -1,17 +1,42 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
-setup(name='Tinder',
-      version='2018.6',
-      description='Tinder API for Python',
-      long_description=open('README.md').read().strip(),
-      author='fabien bessez',
-      #author_email='',
-      url='https://github.com/fbessez/Tinder',
-      py_modules=['tinder_api', 'tinder_api_sms', 'fb_auth_token'],
-      install_requires=['requests',
-                        'robobrowser',
-                        'lxml'],
-      license='MIT License',
+PROJECT = "tinder"
+VERSION = "0.0.0"
+DESC = "Python wrapper for the Tinder API"
+LONG_DESC = open("README.md").read().strip()
+AUTHOR = "Vikash Kothary"
+AUTHOR_EMAIL = "kothary.vikash@gmail.com"
+URL = "https://github.com/Vikash-Kothary/tinder-api-python"
+LICENSE = open("LICENSE").read().strip()
+
+setup(name=PROJECT,
+      version=VERSION,
+      description=DESC,
+      long_description=LONG_DESC,
+      long_description_content_type="text/markdown",
+      author=AUTHOR,
+      author_email=AUTHOR_EMAIL,
+      url=URL,
+      license=LICENSE,
+      packages=find_packages(),
       zip_safe=False,
-      keywords=['tinder-api', 'tinder', 'python-3', 'robobrowser'],
-      #classifiers=[])
+      install_requires=[
+          "requests==2.20.0",
+          "robobrowser==0.5.3",
+          "lxml==4.2.5"
+      ],
+      keywords=["tinder-api", "tinder", "python-3", "robobrowser"],
+      classifiers=[
+          "Programming Language :: Python",
+          "Topic :: Software Development :: Libraries :: Python Modules",
+          "Development Status :: 4 - Beta",
+          "Intended Audience :: Developers",
+          "Intended Audience :: End Users/Desktop",
+          "Operating System :: OS Independent",
+          "License :: OSI Approved :: MIT License",
+          "Programming Language :: Python :: 3.5",
+          "Programming Language :: Python :: 2.7",
+          "Topic :: Internet :: WWW/HTTP",
+          "Environment :: Console"
+      ],
+      )
