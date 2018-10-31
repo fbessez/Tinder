9ad94f87e811e898d452b86e274d52ca1cbca819
Vikash Kothary <kothary.vikash@gmail.com>
2018-10-31 02:50:02 +0000

    Remove separate configurations for testing

--- tinder_api/client.py
+++ tinder_api/client.py
@@ -5,51 +5,51 @@ client.py - API Client
 
 import requests
 import json
-from . import config as c
-from . import utils
-from .errors import AuthenticationError
+from .config import Config
+from .utils import Utils
+from .errors import RequestError
 from .endpoints import Endpoints
 
 
 class Client(Endpoints, object):
     """API Client"""
 
-    def __init__(self, username=None, password=None, config=None):
+    def __init__(self, username=None, password=None):
         """
         """
-        self.config = config
         self.headers = {
             'app_version': '6.9.4',
-            'platform': self.config.OS_PLATFORM,
-            "content-type": self.config.CONTENT_TYPE,
-            "User-agent": self.config.USER_AGENT,
-            "Accept": self.config.ACCEPT,
+            'platform': Config.OS_PLATFORM,
+            "content-type": Config.CONTENT_TYPE,
+            "User-agent": Config.USER_AGENT,
+            "Accept": Config.ACCEPT,
         }
-        self.login(username, password)
+        if username != None and password != None:
+            self.login(username, password)
 
-    def login(self, username=None, password=None):
+    def login(self, username, password):
         """
         """
         # Get Tinder auth token if needed
-        if self.config.TINDER_AUTH_TOKEN == '':
+        if Config.TINDER_AUTH_TOKEN == '':
             # Get facebook token and id if needed
-            if self.config.FB_TOKEN == '' or self.config.FB_ID == '':
+            if Config.FB_TOKEN == '' or Config.FB_ID == '':
                 # Get facebook username and password from params if needed
-                if self.config.FB_USERNAME == '' or self.config.FB_PASSWORD == '':
+                if Config.FB_USERNAME == '' or Config.FB_PASSWORD == '':
                     # Set username and password
-                    self.config.FB_USERNAME = username
-                    self.config.FB_PASSWORD = password
+                    Config.FB_USERNAME = username
+                    Config.FB_PASSWORD = password
                 # Set facebook token and id
-                self.config.FB_TOKEN, self.config.FB_ID = utils.get_facebook_credentials(
-                    self.config.FB_USERNAME, self.config.FB_PASSWORD)
+                Config.FB_TOKEN, Config.FB_ID = Utils.get_facebook_credentials(
+                    Config.FB_USERNAME, Config.FB_PASSWORD)
             # Set tinder auth token
-            self.config.TINDER_AUTH_TOKEN = self.get_auth_token(
-                self.config.FB_TOKEN, self.config.FB_ID)
-        self.headers.update({"X-Auth-Token": self.config.TINDER_AUTH_TOKEN})
+            Config.TINDER_AUTH_TOKEN = self.get_auth_token(
+                Config.FB_TOKEN, Config.FB_ID)
+        self.headers.update({"X-Auth-Token": Config.TINDER_AUTH_TOKEN})
 
     def get_request(self, path):
         """"""
-        r = requests.get(self.config.HOST + path, headers=self.headers)
+        r = requests.get(Config.HOST + path, headers=self.headers)
         if (r.status_code != 200):
             raise RequestError(path)
         return r.json()
@@ -58,10 +58,10 @@ class Client(Endpoints, object):
         """"""
         r = {}
         if params == None:
-            r = requests.post(self.config.HOST + path, headers=self.headers)
+            r = requests.post(Config.HOST + path, headers=self.headers)
         else:
             data = json.dumps(params)
-            r = requests.post(self.config.HOST + path, headers=self.headers, data=data)
+            r = requests.post(Config.HOST + path, headers=self.headers, data=data)
         if (r.status_code != 200):
             raise RequestError(path)
         return r.json()
@@ -69,14 +69,14 @@ class Client(Endpoints, object):
     def put_request(self, path, params):
         """"""
         data = json.dumps(params)
-        r = requests.put(self.config.HOST + path, headers=self.headers, data=data)
+        r = requests.put(Config.HOST + path, headers=self.headers, data=data)
         if (r.status_code != 200):
             raise RequestError(path)
         return r.json()
 
     def delete_request(self, path):
         """"""
-        r = requests.delete(self.config.HOST + path, headers=self.headers)
+        r = requests.delete(Config.HOST + path, headers=self.headers)
         if (r.status_code != 200):
             raise RequestError(path)
         return r.json()
--- tinder_api/config.py
+++ tinder_api/config.py
@@ -4,7 +4,6 @@ config.py -
 """
 
 import os
-import configparser
 from . import utils
 
 
@@ -48,37 +47,6 @@ class Config:
 
     # Facebook Credentials
     FB_AUTH_URL = 'https://www.facebook.com/v2.6/dialog/oauth?redirect_uri=fb464891386855067%3A%2F%2Fauthorize%2F&display=touch&state=%7B%22challenge%22%3A%22IUUkEUqIGud332lfu%252BMJhxL4Wlc%253D%22%2C%220_auth_logger_id%22%3A%2230F06532-A1B9-4B10-BB28-B29956C71AB1%22%2C%22com.facebook.sdk_client_state%22%3Atrue%2C%223_method%22%3A%22sfvc_auth%22%7D&scope=user_birthday%2Cuser_photos%2Cuser_education_history%2Cemail%2Cuser_relationship_details%2Cuser_friends%2Cuser_work_history%2Cuser_likes&response_type=token%2Csigned_request&default_audience=friends&return_scopes=true&auth_type=rerequest&client_id=464891386855067&ret=login&sdk=ios&logger_id=30F06532-A1B9-4B10-BB28-B29956C71AB1&ext=1470840777&hash=AeZqkIcf-NEW6vBd'
-
-
-class DevelopmentConfig(Config):
-    """Loads login credentials from config.ini
-    """
-    ENVIRONMENT = 'DEFAULT'
-
-    config = configparser.ConfigParser()
-    config_file = utils.find_file('config.ini')
-    if config_file:
-        config.read(config_file)
-
-        FB_USERNAME = config.get(ENVIRONMENT, 'FB_USERNAME')
-        FB_PASSWORD = config.get(ENVIRONMENT, 'FB_PASSWORD')
-        FB_TOKEN = config.get(ENVIRONMENT, 'FB_TOKEN')
-        FB_ID = config.get(ENVIRONMENT, 'FB_ID')
-        TINDER_AUTH_TOKEN = config.get(ENVIRONMENT, 'TINDER_AUTH_TOKEN')
-
-    def save():
-        config[ENVIRONMENT]['FB_USERNAME'] = FB_USERNAME
-        config[ENVIRONMENT]['FB_PASSWORD'] = FB_PASSWORD
-        config[ENVIRONMENT]['FB_TOKEN'] = FB_TOKEN
-        config[ENVIRONMENT]['FB_ID'] = FB_ID
-        config[ENVIRONMENT]['TINDER_AUTH_TOKEN'] = TINDER_AUTH_TOKEN
-        with open('config.ini', w) as config_file:
-            config.write(config_file)
-
-
-class CIConfig(Config):
-    """Loads login credentials from environment variables
-    """
     FB_USERNAME = os.getenv('FB_USERNAME', None)
     FB_PASSWORD = os.getenv('FB_PASSWORD', None)
     FB_TOKEN = os.getenv('FB_TOKEN', None)
@@ -86,6 +54,3 @@ class CIConfig(Config):
 
     # Tinder Credentials
     TINDER_AUTH_TOKEN = os.getenv('TINDER_AUTH_TOKEN', None)
-
-    def save():
-        pass
