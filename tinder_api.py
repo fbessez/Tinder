d0ddb6726ed5f21629575879cb232b0df96c6c95
bertolla <patrick.bertolla@gmail.com>
2020-06-11 21:17:16 +0200

    adapt token request to tinder api v2

--- tinder_api.py
+++ tinder_api.py
@@ -18,14 +18,14 @@ def get_auth_token(fb_auth_token, fb_user_id):
         return {"error": "could not retrieve fb_auth_token"}
     if "error" in fb_user_id:
         return {"error": "could not retrieve fb_user_id"}
-    url = config.host + '/auth'
+    url = config.host + '/v2/auth/login/facebook'
     req = requests.post(url,
                         headers=headers,
                         data=json.dumps(
-                            {'facebook_token': fb_auth_token, 'facebook_id': fb_user_id})
+                            {'token': fb_auth_token, 'facebook_id': fb_user_id})
                         )
     try:
-        tinder_auth_token = req.json()["token"]
+        tinder_auth_token = req.json()["data"]["api_token"]
         headers.update({"X-Auth-Token": tinder_auth_token})
         get_headers.update({"X-Auth-Token": tinder_auth_token})
         print("You have been successfully authorized!")
