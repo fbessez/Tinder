ff544af1321dec4214bdb0d6375a791680c99862
maxime.peim <maxime.peim@polytechnique.edu>
2021-11-02 15:45:01 +0100

    Added feature to retrieve matches in v2

--- src/tinder/v2/user.py
+++ src/tinder/v2/user.py
@@ -1,4 +1,29 @@
 from tinder.user import User
 
 class V2User(User):
-    pass
+    
+    @User.need_logged
+    def matches(self, page_token: str = None) -> list:
+        url = v2.MATCHES + '?count=60' + (f'&page_token={page_token}' if page_token else '')
+        r = self._session.get(url)
+        response = r.json()
+
+        if 'data' not in response or \
+            'matches' not in response['data']:
+            raise Exception
+
+        matches = response['data']['matches']
+        next_page_token = response['data'].get('next_page_token', None)
+        return matches, next_page_token
+    
+    @property
+    @User.need_logged
+    def all_matches(self) -> list:
+        matches = []
+        next_page_token = ''
+
+        while next_page_token is not None:
+            next_matches, next_page_token = self.matches(next_page_token)
+            matches += next_matches
+
+        return matches
